7db6e4e175ab6716743422c702c69bb3a685036b
Ishaan <ishaanamahajan@gmail.com>
2023-10-22 16:25:34 -0500

    adding demos for 11

---
+++ wheeled_vehicle_models/11dof/demos/demo_hmmwv_hi.cpp
@@ -0,0 +1,35 @@
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+
+#include "dof11_halfImplicit.h"
+
+using namespace d11;
+
+int main(int argc, char** argv) {
+    // Driver inputs and reference trajectory
+    std::string driver_file = "../data/input/" + std::string(argv[1]) + ".txt";
+
+    // Vehicle specification
+    std::string vehParamsJSON = (char*)"../data/json/HMMWV/vehicle.json";
+    std::string tireParamsJSON = (char*)"../data/json/HMMWV/tmeasy.json";
+
+    // Construct the solver
+    d18SolverHalfImplicit solver;
+    solver.Construct(vehParamsJSON, tireParamsJSON, driver_file);
+
+    // Set time step
+    solver.SetTimeStep(1e-3);
+
+    // Initialize solver (set initial conditions)
+    VehicleState veh_st;
+    TMeasyState tiref_st;
+    TMeasyState tirer_st;
+    
+    solver.Initialize(veh_st, tiref_st, tirer_st);
+
+    // Enable output
+    solver.SetOutput("../data/output/" + std::string(argv[1]) + "_hmmwv18Hi.csv", 100);
+    // Solve without quadratures (no reference path required)
+    solver.Solve();
+}
---
+++ wheeled_vehicle_models/11dof/demos/demo_hmmwv_sundials.cpp
@@ -0,0 +1,38 @@
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+
+#include "dof11_sundials.h"
+
+using namespace d11;
+
+int main(int argc, char** argv) {
+    // Driver inputs and reference trajectory
+    std::string driver_file = "../data/input/" + std::string(argv[1]) + ".txt";
+
+    // Vehicle specification
+    std::string vehParamsJSON = (char*)"../data/json/HMMWV/vehicle.json";
+    std::string tireParamsJSON = (char*)"../data/json/HMMWV/tmeasy.json";
+
+    // Construct the solver
+    d18SolverSundials solver;
+    solver.Construct(vehParamsJSON, tireParamsJSON, driver_file);
+
+    // Set optional inputs
+    solver.SetTolerances(1e-5, 1e-3);
+    solver.SetMaxStep(1e-2);
+
+    // Initialize solver (set initial conditions)
+    VehicleState veh_st;
+    TMeasyState tiref_st;
+    TMeasyState tirer_st;
+   
+    solver.Initialize(veh_st, tiref_st, tirer_st);
+
+    // Enable output
+    solver.SetOutput("../data/output/" + std::string(argv[1]) + "_hmmwv18Sundials.csv");
+    solver.SetVerbose(false);
+
+    // Solve without quadratures (no reference path required)
+    solver.Solve(false);
+}
