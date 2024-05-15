6d420d838fece0564bd1c7c2ec1d9c8700715c92
Huzaifg <huzaifa.unjha@gmail.com>
2024-05-15 14:24:15 -0500

    fixing optional GPU build feature in paper results

--- paper/paper_results/bench.sh
+++ paper/paper_results/bench.sh
@@ -12,71 +12,56 @@ answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 if [ "$answer" == "y" ] || [ "$answer" == "yes" ]; then
     echo "Building with GPU Benchmarks..."
     cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GPU_TESTS=ON ..
+    make -j4
+
+    # Run the GPU benchmarks
+    echo "Running GPU Benchmarks..."
+    echo "11 DOF"
+    ./11dof_gpu_bench 1024 16 > ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 2048 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 4096 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 8192 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 16384 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 32768 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 65536 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 131072 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 262144 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 393216 16 >> ../data/LFDM/11dof.out
+    ./11dof_gpu_bench 524288 16 >> ../data/LFDM/11dof.out
+    echo "18 DOF"
+    ./18dof_gpu_bench 1024 16 > ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 2048 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 4096 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 8192 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 16384 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 32768 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 65536 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 131072 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 262144 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 393216 16 >> ../data/LFDM/18dof.out
+    ./18dof_gpu_bench 524288 16 >> ../data/LFDM/18dof.out
+    echo "24 DOF"
+    ./24dof_gpu_bench 1024 16 > ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 2048 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 4096 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 8192 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 16384 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 32768 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 65536 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 131072 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 262144 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 393216 16 >> ../data/LFDM/24dof.out
+    ./24dof_gpu_bench 524288 16 >> ../data/LFDM/24dof.out
 else
     echo "Building without GPU Benchmarks..."
     cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GPU_TESTS=OFF ..
+    make -j4
 fi
 
-make -j4
-
-# Run the tests
+# Run the CPU benchmarks
 echo "Running CPU Benchmarks..."
 ./accTest
 ./dlTest
 ./timingTest
 
-
-echo "Running GPU Benchmarks..."
-echo "11 DOF"
-./11dof_gpu_bench 1024 16 > ../data/LFDM/11dof.out
-./11dof_gpu_bench 2048 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 4096 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 8192 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 16384 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 32768 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 65536 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 131072 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 262144 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 393216 16 >> ../data/LFDM/11dof.out
-./11dof_gpu_bench 524288 16 >> ../data/LFDM/11dof.out
-echo "18 DOF"
-./18dof_gpu_bench 1024 16 > ../data/LFDM/18dof.out
-./18dof_gpu_bench 2048 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 4096 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 8192 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 16384 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 32768 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 65536 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 131072 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 262144 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 393216 16 >> ../data/LFDM/18dof.out
-./18dof_gpu_bench 524288 16 >> ../data/LFDM/18dof.out
-echo "24 DOF"
-./24dof_gpu_bench 1024 16 > ../data/LFDM/24dof.out
-./24dof_gpu_bench 2048 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 4096 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 8192 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 16384 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 32768 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 65536 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 131072 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 262144 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 393216 16 >> ../data/LFDM/24dof.out
-./24dof_gpu_bench 524288 16 >> ../data/LFDM/24dof.out
-
-
-echo "Plotting CPU Benchmarks..."
-cd ../plotting/
-if [ ! -d "images" ]; then
-    mkdir images
-fi
-python3 accTestPlot.py 1
-python3 dlTestPlot.py 1
-
-echo "Plotting GPU Benchmarks..."
-python3 gpuScalePlot.py 1
-
-echo "Results plotted. See ./plotting/images folder for results."
-
-
-
+# Rest of the code...
